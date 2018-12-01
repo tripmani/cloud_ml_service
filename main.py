43c7dc1ee3164d253b7640ef2ac3db019ffa93c1
manish <mntrp3@gmail.com>
2018-12-01 17:39:25 +0530

    Minor changes

--- main.py
+++ main.py
@@ -15,27 +15,35 @@ X = iris.data
 Y = iris.target
 X_train,X_test,Y_train,Y_test = train_test_split(X, Y, test_size=0.2)
 app = Flask(__name__)
-str1 = '<!doctype html><html lang=''><head><style>body { background: url("/static/cc.png") ;}</style></head><body>'
-str2 = '</body><html>'
 
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
-@app.route('/decisiontree', methods=['GET'])
+def model(model):
+	model.fit(X_train, Y_train)
+	predcition = model.predict(X_test)
+	prediction_int = numpy.around(prediction).astype(int)
+	score = accuracy_score(Y_test, prediction_int)
+	return score
+
+@app.route('/dtree', methods=['GET'])
 def decisiontree():
-    clf = tree.DecisionTreeClassifier()
+    dtree = tree.DecisionTreeClassifier()
+	score = model(dtree)
+'''
     clf = clf.fit(X_train, Y_train)
 
     pred = clf.predict(X_test)
     pred = numpy.around(pred).astype(int)
 
     score = accuracy_score(Y_test,pred)
+'''
 
     return str("Accuracy for Decision Tree : ") + str(score)
 
-@app.route('/linear', methods=['GET'])
+@app.route('/lr', methods=['GET'])
 def linear():
     reger = linear_model.LinearRegression()
     reger.fit(X_train,Y_train)
@@ -46,7 +54,7 @@ def linear():
     score = accuracy_score(Y_test,pred)
     return str("Accuracy for Linear Regression : ") + str(score)
 
-@app.route('/naivebayes', methods=['GET'])
+@app.route('/nb', methods=['GET'])
 def naivebayes():
     gnb = GaussianNB()
     gnb.fit(X_train,Y_train)
@@ -57,21 +65,6 @@ def naivebayes():
     score = accuracy_score(Y_test,pred)
     return str("Accuracy for Naive Bayes : ") + str(score)
 
-'''
-@app.route('/error', methods=['GET'])
-def error():
-    return render_template('error.html')
-
-
-@app.route('/weather', methods=['GET'])
-def get_weather():
-    city = request.args.get('city', None)
-    country = request.args.get('country', None)
-    if weather_handler.run(city, country):
-        return render_template('page2.html')
-    else:
-        return render_template('error.html')
-'''
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8084))
